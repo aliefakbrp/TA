8c67afd430c877964bcdab185d4817c9e920505e
Alief Akbar Purnama <74760595+aliefakbrp@users.noreply.github.com>
2022-11-21 02:21:28 +0700

    Update asem.py

--- asem.py
+++ asem.py
@@ -86,13 +86,13 @@ x_train,x_test,y_train,y_test = train_test_split(scaled_features,y,test_size=0.2
 # a = options
 # st.text('asu')
 fixedacidity=float(st.text_input('asu'))
-fixedacidity=
+# fixedacidity
 
 # totChol = st.text_input(format="%i")
 # st.write(totChol)
 
 
-x_new = [[totChol,	0.28,	0.56,	1.9,	0.075,	17.0,	60.0,	0.99800,	3.16,	0.58,	9.8]]
+x_new = [[fixedacidity,	0.28,	0.56,	1.9,	0.075,	17.0,	60.0,	0.99800,	3.16,	0.58,	9.8]]
 maximal=0
 minimal=1
 for i in range(len(x_new[0])):
