101e04bdcd068168b2fa59ce685b1aa36f2bbc04
Alief Akbar Purnama <74760595+aliefakbrp@users.noreply.github.com>
2022-11-28 08:01:47 +0700

    Update TA.py

--- TA.py
+++ TA.py
@@ -95,21 +95,22 @@ with dataframe:
     dataset, ket = st.tabs(['Dataset', 'Ket Dataset'])
     with ket:
         st.write("""
+                Data : https://www.kaggle.com/datasets/nareshbhat/wine-quality-binary-classification
                 Attribute Information:
-Input variables (based on physicochemical tests):
-1 - fixed acidity
-2 - volatile acidity
-3 - citric acid
-4 - residual sugar
-5 - chlorides
-6 - free sulfur dioxide
-7 - total sulfur dioxide
-8 - density
-9 - pH
-10 - sulphates
-11 - alcohol
-Output variable (based on sensory data):
-12 - quality ('good' and 'bad' based on score >5 and <5)
+                Input variables (based on physicochemical tests):
+                1 - fixed acidity
+                2 - volatile acidity
+                3 - citric acid
+                4 - residual sugar
+                5 - chlorides
+                6 - free sulfur dioxide
+                7 - total sulfur dioxide
+                8 - density
+                9 - pH
+                10 - sulphates
+                11 - alcohol
+                Output variable (based on sensory data):
+                12 - quality ('good' and 'bad' based on score >5 and <5)
                 """)
     with dataset:
         st.dataframe(df)
