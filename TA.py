c1ffecbdad761b57ac762e5d057f85675e3144b6
Alief Akbar Purnama <74760595+aliefakbrp@users.noreply.github.com>
2022-11-28 08:00:43 +0700

    Update TA.py

--- TA.py
+++ TA.py
@@ -95,8 +95,21 @@ with dataframe:
     dataset, ket = st.tabs(['Dataset', 'Ket Dataset'])
     with ket:
         st.write("""
-                Masih belum
-                coming soon
+                Attribute Information:
+Input variables (based on physicochemical tests):
+1 - fixed acidity
+2 - volatile acidity
+3 - citric acid
+4 - residual sugar
+5 - chlorides
+6 - free sulfur dioxide
+7 - total sulfur dioxide
+8 - density
+9 - pH
+10 - sulphates
+11 - alcohol
+Output variable (based on sensory data):
+12 - quality ('good' and 'bad' based on score >5 and <5)
                 """)
     with dataset:
         st.dataframe(df)
