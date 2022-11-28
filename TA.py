8da36e82b4ace530fa3799fd03cbe7d540f1bafa
Alief Akbar Purnama <74760595+aliefakbrp@users.noreply.github.com>
2022-11-28 08:08:37 +0700

    Update TA.py

--- TA.py
+++ TA.py
@@ -99,18 +99,18 @@ with dataframe:
                 * Attribute Information:
                 * Input variables (based on physicochemical tests):
                 * 1 - fixed acidity
-                2 - volatile acidity
-                3 - citric acid
-                4 - residual sugar
-                5 - chlorides
-                6 - free sulfur dioxide
-                7 - total sulfur dioxide
-                8 - density
-                9 - pH
-                10 - sulphates
-                11 - alcohol
+                * 2 - volatile acidity
+                * 3 - citric acid
+                * 4 - residual sugar
+                * 5 - chlorides
+                * 6 - free sulfur dioxide
+                * 7 - total sulfur dioxide
+                * 8 - density
+                * 9 - pH
+                * 10 - sulphates
+                * 11 - alcohol
                 Output variable (based on sensory data):
-                12 - quality ('good' and 'bad' based on score >5 and <5)
+                * 12 - quality 
                 """)
     with dataset:
         st.dataframe(df)
