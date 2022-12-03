0561ebfa9760dd71059e5a7fc8200466fd6aebe9
Alief Akbar Purnama <74760595+aliefakbrp@users.noreply.github.com>
2022-12-03 20:33:53 +0700

    Update TA1.py

--- TA1.py
+++ TA1.py
@@ -17,9 +17,6 @@ x = df.iloc[:, :-1]
 y = df.loc[:, "quality"]
 y = df['quality'].values
 
-# split data
-from sklearn.model_selection import train_test_split
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=1)
 
 #  normalisasi
 scaler = sklearn.preprocessing.MinMaxScaler()
