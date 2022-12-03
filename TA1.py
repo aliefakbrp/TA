90358a5f8865f3b4c1f3fea74daf4c7bd8a15ca4
Alief Akbar Purnama <74760595+aliefakbrp@users.noreply.github.com>
2022-12-03 21:09:21 +0700

    Update TA1.py

--- TA1.py
+++ TA1.py
@@ -145,7 +145,7 @@ with modeling:
         from sklearn.metrics import accuracy_score
         from sklearn.metrics import make_scorer, accuracy_score,precision_score
         from sklearn.metrics import classification_report
-        from sklearn.metrics import confusion_matrix
+#         from sklearn.metrics import confusion_matrix
         from sklearn.metrics import precision_score,recall_score,f1_score
         from sklearn.preprocessing import LabelEncoder
 
